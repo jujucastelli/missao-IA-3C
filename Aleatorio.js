fc687e1c8e02ac3dd82498a4be983d5676ebc7b9
jujucastelli <130070249+jujucastelli@users.noreply.github.com>
2024-09-19 07:51:24 -0300

    Create Aleatorio.js

---
+++ Aleatorio.js
@@ -0,0 +1,18 @@
+const nomes = ["Juca", "Giuliana"]
+
+export function aleatorio (lista){
+    const posicao = Math.floor(Math.random()* lista.lenght);
+    return lista[posicao];
+}
+
+export const nome = aleatorio(nomes)
+
+
+const nomes = ["Juca", "Giuliana"]
+
+export function aleatorio (lista){
+    const posicao = Math.floor(Math.random()* lista.lenght);
+    return lista[posicao];
+}
+
+export const nome = aleatorio(nomes)
